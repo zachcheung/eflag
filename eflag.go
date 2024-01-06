b88c8bfd901db3bdd278a4de39a17062844db42e
Zach Cheung <kuroro.zhang@gmail.com>
2024-01-06 11:28:19 +0800

    update Flag struct

--- eflag.go
+++ eflag.go
@@ -8,35 +8,36 @@ import (
 )
 
 type Flag struct {
-	Name         string
-	DefaultValue interface{}
-	Usage        string
-	Value        interface{}
-	Changed      bool
+	Name    string
+	Flag    *flag.Flag
+	Changed bool
 }
 
-func (f *Flag) Init() {
-	switch f.DefaultValue.(type) {
+func newFlag(p interface{}, name string, value interface{}, usage string) *Flag {
+	switch value.(type) {
 	case bool:
-		flag.BoolVar(f.Value.(*bool), f.Name, f.DefaultValue.(bool), f.Usage)
+		flag.BoolVar(p.(*bool), name, value.(bool), usage)
 	case time.Duration:
-		flag.DurationVar(f.Value.(*time.Duration), f.Name, f.DefaultValue.(time.Duration), f.Usage)
+		flag.DurationVar(p.(*time.Duration), name, value.(time.Duration), usage)
 	case float64:
-		flag.Float64Var(f.Value.(*float64), f.Name, f.DefaultValue.(float64), f.Usage)
+		flag.Float64Var(p.(*float64), name, value.(float64), usage)
 	case int:
-		flag.IntVar(f.Value.(*int), f.Name, f.DefaultValue.(int), f.Usage)
+		flag.IntVar(p.(*int), name, value.(int), usage)
 	case int64:
-		flag.Int64Var(f.Value.(*int64), f.Name, f.DefaultValue.(int64), f.Usage)
+		flag.Int64Var(p.(*int64), name, value.(int64), usage)
 	case string:
-		flag.StringVar(f.Value.(*string), f.Name, f.DefaultValue.(string), f.Usage)
+		flag.StringVar(p.(*string), name, value.(string), usage)
 	case uint:
-		flag.UintVar(f.Value.(*uint), f.Name, f.DefaultValue.(uint), f.Usage)
+		flag.UintVar(p.(*uint), name, value.(uint), usage)
 	case uint64:
-		flag.Uint64Var(f.Value.(*uint64), f.Name, f.DefaultValue.(uint64), f.Usage)
+		flag.Uint64Var(p.(*uint64), name, value.(uint64), usage)
 	default:
-		fmt.Printf("invalid type: %T", f.DefaultValue)
+		fmt.Printf("invalid type: %T", value)
 		os.Exit(1)
 	}
+
+	return &Flag{Name: name, Flag: flag.Lookup(name)}
+
 }
 
 type Flags []*Flag
@@ -44,7 +45,6 @@ type Flags []*Flag
 func (ff Flags) Parse() {
 	m := make(map[string]*Flag)
 	for _, f := range ff {
-		f.Init()
 		m[f.Name] = f
 	}
 	flag.Parse()
@@ -59,12 +59,7 @@ func (ff Flags) Parse() {
 var flags Flags
 
 func Var(p interface{}, name string, value interface{}, usage string) {
-	flags = append(flags, &Flag{
-		Value:        p,
-		Name:         name,
-		DefaultValue: value,
-		Usage:        usage,
-	})
+	flags = append(flags, newFlag(p, name, value, usage))
 }
 
 func Parse() {
